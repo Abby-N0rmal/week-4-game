b8d7fba34fe23c61661b73591b62d6a781ae2570
Amber Girard <rhadorasrequiem@gmail.com>
2018-02-12 11:09:26 -0500

    new files

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//Creating a giant crystalGame object that will house all of the logic and variables.
+var crystalGame = {
+
+	// Variables that set the intial state of the crystal game.
+	randomNumber: null,
+	sumofCrystals: 0,
+	crystalNumber: 0,
+	wins: 0,
+	losses: 0,
+
+	// The setupGame method is called when the page first loads.
+	setupGame: function() {
+		//Here we pick a random number to match with the cyrstals.
+		var objKeys = Object.keys(this.randomNumber);
+		this.
+	}
+	}
