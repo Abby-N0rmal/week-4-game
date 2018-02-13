247ae22ab6e167ee9ca75ceddeb2a81fc963b6d9
Amber Girard <rhadorasrequiem@gmail.com>
2018-02-13 15:03:03 -0500

    tweaks before adding the js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,11 @@
-//Creating a giant crystalGame object that will house all of the logic and variables.
-var crystalGame = {
+$(document).ready(function() {
 
-	// Variables that set the intial state of the crystal game.
-	randomNumber: null,
-	sumofCrystals: 0,
-	crystalNumber: 0,
-	wins: 0,
-	losses: 0,
+	var matchingNumber = 0;
 
-	// The setupGame method is called when the page first loads.
-	setupGame: function() {
-		//Here we pick a random number to match with the cyrstals.
-		var objKeys = Object.keys(this.randomNumber);
-		this.
-	}
-	}
+	var randomNumber = randomNumGen();
+
+	var wins = 0;
+	var losses= 0;
+	var crystals;
+
+	
