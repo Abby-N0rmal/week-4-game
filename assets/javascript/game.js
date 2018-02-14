0d799fd38fd04780c84bf8d2e994ab9925994b4b
Amber Girard <rhadorasrequiem@gmail.com>
2018-02-13 22:09:04 -0500

    idk I just can't get it to work. deleted it. sorry.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,4 +8,105 @@ $(document).ready(function() {
 	var losses= 0;
 	var crystals;
 
+	function randomNumCrystals() {
+		return{
+			purple: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: "assets/images/purple.jpg"
+			};
+			blue: {
+				points: Math.floor(Math.random() * 12) + 1,
+				iamgeUrl: "assets/images/blue.jpg"
+			};
+			green: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: "assets/images/green.jpg"
+			};
+			red: {
+				points: Math.floor(Math.random() * 12) + 1,
+				imageUrl: "assets/images/green.jpg"
+			}
+		};
+	}
 	
+	function randomNumGen() {
+		return Math.floor(Math.random() * 102) + 19;
+	}
+
+	function setGame() {
+		yourmatchingNumber = 0;
+		crystals = randomNumCrystals();
+		randomNum = randomNumGen();
+		$("#random-area").text(randomNum);
+	}
+
+	function updateDom(didUserWin) {
+		$("#win-area").text(randomNum);
+
+		if (didUserWin === true) {
+			$("#win-area").append($("<p>").text("You Won!"));
+			setGame();
+			renderMatchingNumber();
+		}
+
+		else if (didUserWin === false) {
+			$("#win-area").append($("<p>").text("You Lost!"));
+			setGame();
+			renderMatchingNumber();
+		}
+
+		var wSpan = $("<span>").text(wins);
+		var lSpan = $("<span>").text(losses);
+
+		var pWins = $("<p>").text("Wins: ");
+		var pLosses = $("<p>").text("Losses: ");
+
+		pWins.append(wSpan);
+		pLosses.append(lSpan);
+
+		$("#win-area").append(pWins);
+		$("win-area").append(pLosses);
+	}
+
+	function renderCrystals() {
+		for (var key in crystals) {
+			var crystalDiv = $("<div class='crystals-button' data-name='" + key + "'>");
+			var crystalImg = $("<img alt'image' class='crystal-img'>").attr("src", crystals[key].imageUrl);
+			crystalDiv.append(crystalImg);
+			$("#crystal-area").append(crystalDiv);
+		}
+	}
+
+	function updateMatchingNumber(crystal) {
+		var scoreNumDiv = $("<div id='score-number'>").text("data-name")].points;
+	}
+
+	function renderMatchingNumber() {
+		var scoreNumDiv = $("div id='score-number'>").text(yourMatchingNumber);
+		$("#score-area").html();
+		$("#score-area").html(scoreNumDiv);
+	}
+
+	setGame();
+	updateDom();
+	renderCrystals();
+	renderMatchingNumber();
+
+	$(".crystals-button").on("click", function(event) {
+		updateMatchingNumber($(this));
+		renderMatchingNumber();
+
+		if (yourMatchingNumber === randomNum) {
+			wins++;
+			setGame();
+			updateDom(true);
+		}
+
+		else if (yourMatchingNumber > randomNum) {
+			losses++;
+			setGame();
+			updateDom(false);
+		}
+	});
+
+});
